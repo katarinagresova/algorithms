fed7882fc11e177a0bf755dadc16c187ca5d2290
Katarína Grešová <514001@mail.muni.cz>
2020-12-04 15:02:54 +0100

    solution for part 2

--- day2/solution.py
+++ day2/solution.py
@@ -9,7 +9,7 @@ def parse_line(passw_line):
     return int(m[0]), int(m[1]), m[2], m[3]
 
 
-def is_passw_valid(passw_line):
+def is_passw_valid_part1(passw_line):
     lower_limit, upper_limit, character, passw = parse_line(passw_line)
 
     regex = r'' + character
@@ -18,20 +18,37 @@ def is_passw_valid(passw_line):
     return lower_limit <= len(m) <= upper_limit
 
 
-def count_valid_passw():
+def is_passw_valid_part2(passw_line):
+    first_position, second_position, character, passw = parse_line(passw_line)
+
+    return (passw[first_position - 1] == character and passw[second_position - 1] != character) \
+        or (passw[first_position - 1] != character and passw[second_position - 1] == character)
+
+
+def count_valid_passw(part1=True):
     counter = 0
     with open(INPUT_FILE, 'r') as f:
         for line in f.readlines():
-            if is_passw_valid(line):
-                counter += 1
+            if part1:
+                if is_passw_valid_part1(line):
+                    counter += 1
+            else:
+                if is_passw_valid_part2(line):
+                    counter += 1
 
     return counter
 
 
 def run_part1():
-    count = count_valid_passw()
+    count = count_valid_passw(part1=True)
+    print("Number of valid passwords:", count)
+
+
+def run_part2():
+    count = count_valid_passw(part1=False)
     print("Number of valid passwords:", count)
 
 
 if __name__ == '__main__':
     run_part1()
+    run_part2()
