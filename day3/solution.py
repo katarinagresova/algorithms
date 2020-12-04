1fe8f84cdc9b06d3818a440ce477efa87bc069a8
Katarína Grešová <514001@mail.muni.cz>
2020-12-04 15:38:02 +0100

    solution for part 2

--- day3/solution.py
+++ day3/solution.py
@@ -1,7 +1,5 @@
 INPUT_FILE = 'input.txt'
 TREE_SYMBOL = '#'
-SLOPE_RIGHT = 3
-SLOPE_DOWN = 1
 
 
 def read_file():
@@ -11,7 +9,7 @@ def read_file():
     return lines
 
 
-def count_trees():
+def count_trees(slope_right, slope_down):
     area = read_file()
 
     x = 0
@@ -21,20 +19,35 @@ def count_trees():
     width = len(area[0])
 
     while y < height:
-        if y == 322:
-            print("hey")
         if area[y][x % width] == TREE_SYMBOL:
             counter += 1
-        x += SLOPE_RIGHT
-        y += SLOPE_DOWN
+        x += slope_right
+        y += slope_down
 
     return counter
 
 
 def run_part1():
-    count = count_trees()
+    count = count_trees(slope_right=3, slope_down=1)
     print('Encountered trees:', count)
+
+
+def run_part2():
+    slopes = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]
+    ]
+    counter = 1
+
+    for slope in slopes:
+        counter *= count_trees(slope_right=slope[0], slope_down=slope[1])
+
+    print('Encountered trees on all slopes multiplied:', counter)
 
 
 if __name__ == '__main__':
     run_part1()
+    run_part2()
