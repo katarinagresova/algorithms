45674655d8a9753ac6630455b57d99679c799d03
Katarína Grešová <514001@mail.muni.cz>
2020-12-04 14:13:22 +0100

    solution for part 2

--- day1/solution.py
+++ day1/solution.py
@@ -14,5 +14,16 @@ def run_part1():
                 break
 
 
+def run_part2():
+    for i in range(0, len(numbers) - 1):
+        for j in range(i + 1, len(numbers) - 1):
+            for k in range(j + 1, len(numbers) - 1):
+                if numbers[i] + numbers[j] + numbers[k] == TARGET_SUM:
+                    print('Numbers are: ', numbers[i], numbers[j], numbers[k])
+                    print('Multiplication equals:', numbers[i] * numbers[j] * numbers[k])
+                    break
+
+
 if __name__ == '__main__':
     run_part1()
+    run_part2()
