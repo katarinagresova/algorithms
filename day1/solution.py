7b055db134f4b23d3d96f1311e1099c60fa58b9d
Katarína Grešová <514001@mail.muni.cz>
2020-12-04 14:10:20 +0100

    solution for part1 of puzzle

---
+++ day1/solution.py
@@ -0,0 +1,18 @@
+input_file = 'input.txt'
+TARGET_SUM = 2020
+
+with open(input_file) as f:
+    numbers = [int(line.rstrip()) for line in f]
+
+
+def run_part1():
+    for i in range(0, len(numbers) - 1):
+        for j in range(i + 1, len(numbers) - 1):
+            if numbers[i] + numbers[j] == TARGET_SUM:
+                print('Numbers are: ', numbers[i], numbers[j])
+                print('Multiplication equals:', numbers[i] * numbers[j])
+                break
+
+
+if __name__ == '__main__':
+    run_part1()
