e8df441ff969ede3da790601bf7ba7dd60299949
katarinagresova <514001@mail.muni.cz>
2022-12-02 10:34:06 +0100

    day 1 part 2

--- AdventOfCode2022/day1/solution.py
+++ AdventOfCode2022/day1/solution.py
@@ -11,7 +11,23 @@ def run_part1():
                 sum = 0
             else:
                 sum += int(line.rstrip())
+    max_sum = max(max_sum, sum)
     print('Max number of calories carried by single Elf is:', max_sum)
+
+def run_part2():
+    sums = []
+    with open(input_file) as f:
+        sum = 0
+        for line in f:
+            if line.rstrip() == '':
+                sums.append(sum)
+                sum = 0
+            else:
+                sum += int(line.rstrip())
+    sums.append(sum)
+    sums = sorted(sums)
+    print('Max number of calories carried by top thee Elf are:', sums[-1] + sums[-2] + sums[-3])
 
 if __name__ == '__main__':
     run_part1()
+    run_part2()
